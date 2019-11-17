cb09460d8ea9f994ebe70f3f29bf1c44b2807da5
Your Name <you@example.com>
2019-11-17 16:43:31 +0100

    refacotoring, find existing files

--- getmp3.sh
+++ getmp3.sh
@@ -1,24 +1,23 @@
 #/bin/sh
 echo "usage  ./getmp3.sh file.html mp_type "
 
-#for file in $(grep -Eo "http:.*.$2" $1 | grep $2  | sed 's/\"//g'| cut -d '>' -f1 | grep cdn)
+#for url in $(grep -Eo "http:.*.$2" $1 | grep $2  | sed 's/\"//g'| cut -d '>' -f1 | grep cdn)
 #TODO duppletten weg ohne sort
-#for file in $(grep -Eo "http:.*.$2" $1 | grep $2  | sed 's/\"//g'| cut -d '>' -f1 | grep cdn | sort -u)
-#for file in $( grep -Eo "https:.*.$2" $1)
+#for url in $(grep -Eo "http:.*.$2" $1 | grep $2  | sed 's/\"//g'| cut -d '>' -f1 | grep cdn | sort -u)
+#for url in $( grep -Eo "https:.*.$2" $1)
 # sed  's/url/\n/g' ../1000antworten.xml > 1000antworten.txt 
 # sed  's/url/\n/g' wissen.xml > wissen.txt 
 #  split -l 500 1000antworten3.txt 
 
 
-# for file in $( grep -Eo "https:.*.$2" $1 | sort -u)
-for file in $( grep -Eo "https:.*.$2" $1 )
+# for url in $( grep -Eo "https:.*.$2" $1 | sort -u)
+for url in $( grep -Eo "https:.*.$2" $1 )
 
- #find -type f -name "wo-ist-das-zentrum-unseres-universums.6444m.mp3" | grep -q "."
+ 
 
-
-#for file in $(grep $2 $1 )
+#for url in $(grep $2 $1 )
   do
-  #echo  ${file:0:${#file}-1}
+  #echo  ${url:0:${#url}-1}
 
     while [ `/bin/ps h -C "wget" | /usr/bin/wc -l`  -gt 7 ]
      do
@@ -27,7 +26,9 @@ for file in $( grep -Eo "https:.*.$2" $1 )
        #/bin/ps h -C "wget"
      done
 
-      echo  $file |  grep -oE "[^/]+$"
-      #wget $file &
+      file = $url |  grep -oE "[^/]+$"
+      if (find -type f -name $file| grep -q ".")
+       eoch $file
+       #wget $url &
 
   done
